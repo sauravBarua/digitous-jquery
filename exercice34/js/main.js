8883565dd20c10383f863fc59dad9ac4a55f4dd0
Saurav Barua <myallmailofficial@gmail.com>
2021-01-13 16:55:17 +0100

    add exe34

--- exercice34/js/main.js
+++ exercice34/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     $("button").click(function() {
         $.ajax({
-            url: 'https://restcountries.eu/rest/v2/name/usa',
+            url: `https://restcountries.eu/rest/v2/name/${$("input").val()}`,
             success: function(data, statuts, response) {
                 $("#country").html(data[0].name);
                 $("#capital").html(data[0].capital);
